c5fca1d6d788227c4aae7ca42323d9ab9740437d
LMO <lam@cphbusiness.dk>
2015-11-22 11:01:04 +0100

    Fixing Intercept Error Handling

--- semesterSeedSP/web/app/components/authentication.js
+++ semesterSeedSP/web/app/components/authentication.js
@@ -105,19 +105,13 @@ angular.module('myApp.security', [])
       },
       responseError: function (rejection) {
         var err = rejection.data;
-        if(err == null){
-          $rootScope.error = "Unknown error while trying to connect to the server";
-          var err= "\n########################################################################\n"+
-          "Unknown error while trying to connect to the server\n"+
-          "Did you execute the AngularSeedClient project, and not The AngularSeedServer Project?\n"+
-          "#######################################################################\n";
-          throw err;
-          return;
+        if(typeof err.error === 'undefined' || typeof err.error.code === 'undefined'){
+          return rejection;
         }
-        $rootScope.error = err.error.code +": ";
+       $rootScope.error = err.error.code +": ";
         
         if (err.error.code === 401) {
-          $rootScope.error += " You are are not Authenticated - did you log on to the system"
+          $rootScope.error += " You are are not Authenticated - did you log on to the system";
         }
         else{
            $rootScope.error +=  err.error.message;
